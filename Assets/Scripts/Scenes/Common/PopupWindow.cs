28d393c82f30611f737d0d0602dc439cf92360e1
hirano-d-titech <hirano.d.ae@m.titech.ac.jp>
2025-05-13 08:56:45 +0900

    feat: PopupManager

---
+++ Assets/Scripts/Scenes/Common/PopupWindow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Scenes.Common
+{
+    public abstract class PopupWindow : MonoBehaviour
+    {
+        [SerializeField] private RectTransform safeRect, fixedRect;
+        [SerializeField] private RectTransform outOfWindowRect;
+
+        public virtual void OnOpenWindow()
+        {
+            // play se
+        }
+
+        public void SetupWithCanvas(AutoAspectCanvas canvas)
+        {
+            canvas.AddSafeRectTransform(safeRect);
+            canvas.AddFixedRectTransform(fixedRect);
+            canvas.AddFullRectTransform(outOfWindowRect);
+        }
+
+        public void UnsetFromCanvas(AutoAspectCanvas canvas)
+        {
+            canvas.RemoveSafeRectTransform(safeRect);
+            canvas.RemoveFixedRectTransform(fixedRect);
+            canvas.RemoveFullRectTransform(outOfWindowRect);
+        }
+        
+        public virtual bool EnableClosingByButton()
+        {
+            return true;
+        }
+
+        public virtual void CloseWindow()
+        {
+            if (PopupManager.Instance.ClosePopup(this))
+            {
+                // play se
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/Common/SingleButtonPopup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Scenes.Common
+{
+    public class SingleButtonPopup : PopupWindow
+    {
+        private static readonly Vector2 DefaultSize = new(640f, 320f);
+        
+        [SerializeField] private RectTransform windowRect;
+        [SerializeField] private TextMeshProUGUI descriptionText, okText;
+        private Action _onClosingCallback;
+
+        public void Init(string description, string ok, Action callback)
+        {
+            descriptionText.text = description;
+            okText.text = ok;
+            _onClosingCallback = callback;
+            
+            descriptionText.ForceMeshUpdate();
+            windowRect.sizeDelta = DefaultSize + Vector2.up * descriptionText.GetRenderedValues(false).y;
+        }
+        
+        public override bool EnableClosingByButton()
+        {
+            return false;
+        }
+
+        public override void CloseWindow()
+        {
+            base.CloseWindow();
+            _onClosingCallback?.Invoke();
+        }
+    }
+}
