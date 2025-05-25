82d777ff359aeddc26f30b19bda194f50818c4e4
hirano-d-titech <hirano.d.ae@m.titech.ac.jp>
2025-05-26 03:08:45 +0900

    feat: ボタンの表示形式をより扱いやすくするためのクラス

---
+++ Assets/Scripts/Presentation/Views/Extensions/ButtonWithLabel.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Presentation.Views.Extensions
+{
+    public class ButtonWithLabel : MonoBehaviour
+    {
+        [SerializeField] private RectTransform rectTransform;
+        [SerializeField] private ButtonRP button;
+        [SerializeField] private TextMeshProUGUI label;
+        
+        public Button Button => button;
+        public RectTransform RectTransform => rectTransform;
+        public TextMeshProUGUI Label => label;
+    }
+}
