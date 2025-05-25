8dd40be53896eb7485e29b019b4aa3b70f42828f
hirano-d-titech <hirano.d.ae@m.titech.ac.jp>
2025-05-26 03:14:51 +0900

    feat: サンプルシーンの開始時処理

---
+++ Assets/Scripts/Presentation/Views/Sample/SampleSceneManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Presentation.Views.Sample
+{
+    public class SampleSceneManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] deleteInitial;
+
+        private void Awake()
+        {
+            foreach (var obj in deleteInitial)
+            {
+                Destroy(obj);
+            }
+        }
+    }
+}
