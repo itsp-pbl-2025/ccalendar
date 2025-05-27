c947e15554224d077908465815e74b3f429ea8c8
hirano-d-titech <hirano.d.ae@m.titech.ac.jp>
2025-05-27 12:20:22 +0900

    feat: namespace関係表に説明を入れた

---
+++ Assets/Scripts/Presentation/Views/Extensions/EnumActionAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Presentation.Views.Extensions
+{
+
+    /// <summary>
+    /// Mark a method with an integer argument with this to display the argument as an enum popup in the UnityEvent
+    /// drawer. Use: [EnumAction(typeof(SomeEnumType))]
+    /// From https://forum.unity.com/threads/ability-to-add-enum-argument-to-button-functions.270817
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class EnumActionAttribute : PropertyAttribute
+    {
+        public Type EnumType { get; }
+        public EnumActionAttribute(Type enumType) => EnumType = enumType;
+    }
+}
---
+++ Assets/Scripts/Presentation/Views/Popup/RectFigurePopup.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Presentation.Views.Popup
+{
+    public class RectFigurePopup : PopupWindow
+    {
+        // right, top, left, bottom
+        private static readonly Vector4 Margin = new Vector4(40f, 40f, 40f, 40f);
+        
+        [SerializeField] private RectTransform figureParent, windowRect;
+        
+        public void Init(RectTransform rectTransform)
+        {
+            var size = rectTransform.sizeDelta;
+            // ウィンドウのサイズを設定
+            windowRect.sizeDelta = new Vector2(Margin.x + Margin.z, Margin.y + Margin.w) + size;
+            var figure = Instantiate(rectTransform, figureParent);
+        }
+    }
+}
