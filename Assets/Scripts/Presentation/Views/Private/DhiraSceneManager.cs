ccc0e551bc21f786323d42a190c954e5e186c967
hirano-d-titech <hirano.d.ae@m.titech.ac.jp>
2025-06-10 15:52:57 +0900

    asset: 自分のデバッグ用のシーンを追加した

---
+++ Assets/Scripts/Presentation/Views/Private/DhiraSceneManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Presentation.Presenter;
+using Presentation.Views.Popup;
+using TMPro;
+using UnityEngine;
+
+namespace Presentation.Views.Private
+{
+    public class DhiraSceneManager : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI dateText;
+        
+        private void Start()
+        {
+            OnDateSelected(default);
+        }
+
+        public void OpenDateOnlyPopup()
+        {
+            var popup = InAppContext.Prefabs.GetPopup<DateOnlyPopup>();
+            var window = PopupManager.Instance.ShowPopup(popup);
+            window.Init(OnDateSelected);
+        }
+
+        private void OnDateSelected(DateTime dt)
+        {
+            dateText.text = dt == default ? "" : dt.ToString("yyyy年MM月dd日");
+        }
+    }
+}
