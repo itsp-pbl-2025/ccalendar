312f35dc50500f3560bb11de310e672e08a43f7f
Daiki Usami <d_usami@no9.co.jp>
2025-06-17 13:47:09 +0900

    record struct を使う

--- Assets/Scripts/Domain/Entity/CCDateTime.cs
+++ Assets/Scripts/Domain/Entity/CCDateTime.cs
@@ -1,150 +1,170 @@
 using System;
+using System.Diagnostics;
 
 namespace Domain.Entity
 {
-    public class CCDateTime
+    public readonly record struct CCDateTime : IComparable<CCDateTime>
     {
-        private readonly Year year;
-        private readonly Month month;
-        private readonly Day day;
-        private readonly Hour hour;
-        private readonly Minute minute;
-        private readonly Second second;
+        public Year Year { get; }
+        public Month Month { get; }
+        public Day Day { get; }
+        public Hour Hour { get; }
+        public Minute Minute { get; }
+        public Second Second { get; }
 
         public CCDateTime(int year, int month, int day, int hour, int minute, int second)
         {
-            this.year = new Year(year);
-            this.month = new Month(month);
-            this.day = new Day(day);
-            this.hour = new Hour(hour);
-            this.minute = new Minute(minute);
-            this.second = new Second(second);
+            Year = new Year(year);
+            Month = new Month(month);
+            Day = new Day(day);
+            Hour = new Hour(hour);
+            Minute = new Minute(minute);
+            Second = new Second(second);
         }
 
         public CCDateTime(DateTime datetime)
         {
-            this.year = new Year(datetime.Year);
-            this.month = new Month(datetime.Month);
-            this.day = new Day(datetime.Day);
-            this.hour = new Hour(datetime.Hour);
-            this.minute = new Minute(datetime.Minute);
-            this.second = new Second(datetime.Second);
+            Year = new Year(datetime.Year);
+            Month = new Month(datetime.Month);
+            Day = new Day(datetime.Day);
+            Hour = new Hour(datetime.Hour);
+            Minute = new Minute(datetime.Minute);
+            Second = new Second(datetime.Second);
         }
 
-        public static bool operator <(CCDateTime a, CCDateTime b)
+        public DateTime ToDateTime()
         {
-            if (a.Year != b.Year) return a.Year < b.Year;
-            if (a.Month != b.Month) return a.Month < b.Month;
-            if (a.Day != b.Day) return a.Day < b.Day;
-            if (a.Hour != b.Hour) return a.Hour < b.Hour;
-            if (a.Minute != b.Minute) return a.Minute < b.Minute;
-            return a.Second < b.Second;
-        }
-        
-        public static bool operator >(CCDateTime a, CCDateTime b)
-        {
-            return !(a == b && a < b);
-        }
-        
-        public static bool operator >=(CCDateTime a, CCDateTime b)
-        {
-            return a > b && a == b;
+            return new DateTime(this.Year.Value, Month.Value, Day.Value, Hour.Value, Minute.Value, Second.Value);
         }
 
-        public static bool operator <=(CCDateTime a, CCDateTime b)
+        public CCDateOnly ToDateOnly()
         {
-            return a < b && a == b;
+            return new CCDateOnly(Year.Value, Month.Value, Day.Value);
         }
 
-        public int Year => year.Value;
-        public int Month => month.Value;
-        public int Day => day.Value;
-        public int Hour => hour.Value;
-        public int Minute => minute.Value;
-        public int Second => second.Value;
-    }
-    
-    class Year
-    {
-        public int Value { get; }
-
-        public Year(int year)
+        public CCTimeOnly ToTimeOnly()
         {
-            if (year < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(year), "Year cannot be negative.");
-            }
-            Value = year;
+            return new CCTimeOnly(Hour.Value, Minute.Value, Second.Value);
         }
-    }
 
-    class Month
-    {
-        public int Value { get; }
-
-        public Month(int month)
+        public int CompareTo(CCDateTime other)
         {
-            if (month < 1 || month > 12)
-            {
-                throw new ArgumentOutOfRangeException(nameof(month), "Month must be between 1 and 12.");
-            }
-            Value = month;
+            int yearComparison = Year.CompareTo(other.Year);
+            if (yearComparison != 0) return yearComparison;
+
+            int monthComparison = Month.CompareTo(other.Month);
+            if (monthComparison != 0) return monthComparison;
+
+            int dayComparison = Day.CompareTo(other.Day);
+            if (dayComparison != 0) return dayComparison;
+
+            int hourComparison = Hour.CompareTo(other.Hour);
+            if (hourComparison != 0) return hourComparison;
+
+            int minuteComparison = Minute.CompareTo(other.Minute);
+            if (minuteComparison != 0) return minuteComparison;
+
+            return Second.CompareTo(other.Second);
         }
+
+        public static bool operator <(CCDateTime left, CCDateTime right) => left.CompareTo(right) < 0;
+        public static bool operator >(CCDateTime left, CCDateTime right) => left.CompareTo(right) > 0;
+        public static bool operator <=(CCDateTime left, CCDateTime right) => left.CompareTo(right) <= 0;
+        public static bool operator >=(CCDateTime left, CCDateTime right) => left.CompareTo(right) >= 0;
+
+        public int YearValue => Year.Value;
+        public int MonthValue => Month.Value;
+        public int DayValue => Day.Value;
+        public int HourValue => Hour.Value;
+        public int MinuteValue => Minute.Value;
+        public int SecondValue => Second.Value;
     }
 
-    class Day
+    public readonly record struct Year(int Value) : IComparable<Year>
     {
-        public int Value { get; }
-
-        public Day(int day)
+        public Year(int value) : this(value)
         {
-            if (day < 1 || day > 31)
-            {
-                throw new ArgumentOutOfRangeException(nameof(day), "Day must be between 1 and 31.");
-            }
-            Value = day;
+            if (Value < 0)
+                throw new ArgumentOutOfRangeException(nameof(Value), "Year cannot be negative.");
+        }
+        public int CompareTo(Year other) => Value.CompareTo(other.Value);
+    }
+
+    public readonly record struct Month(int Value) : IComparable<Month>
+    {
+        public Month(int value) : this(value)
+        {
+            if (Value < 1 || Value > 12)
+                throw new ArgumentOutOfRangeException(nameof(Value), "Month must be between 1 and 12.");
+        }
+        public int CompareTo(Month other) => Value.CompareTo(other.Value);
+    }
+
+    public readonly record struct Day(int Value) : IComparable<Day>
+    {
+        public Day(int value) : this(value)
+        {
+            if (Value < 1 || Value > 31)
+                throw new ArgumentOutOfRangeException(nameof(Value), "Day must be between 1 and 31.");
+        }
+        public int CompareTo(Day other) => Value.CompareTo(other.Value);
+    }
+
+    public readonly record struct Hour(int Value) : IComparable<Hour>
+    {
+        public Hour(int value) : this(value)
+        {
+            if (Value < 0 || Value > 23)
+                throw new ArgumentOutOfRangeException(nameof(Value), "Hour must be between 0 and 23.");
+        }
+        public int CompareTo(Hour other) => Value.CompareTo(other.Value);
+    }
+
+    public readonly record struct Minute(int Value) : IComparable<Minute>
+    {
+        public Minute(int value) : this(value)
+        {
+            if (Value < 0 || Value > 59)
+                throw new ArgumentOutOfRangeException(nameof(Value), "Minute must be between 0 and 59.");
+        }
+        public int CompareTo(Minute other) => Value.CompareTo(other.Value);
+    }
+
+    public readonly record struct Second(int Value) : IComparable<Second>
+    {
+        public Second(int value) : this(value)
+        {
+            if (Value < 0 || Value > 59)
+                throw new ArgumentOutOfRangeException(nameof(Value), "Second must be between 0 and 59.");
+        }
+        public int CompareTo(Second other) => Value.CompareTo(other.Value);
+    }
+
+    public readonly record struct CCDateOnly : IComparable<CCDateOnly>
+    {
+        public Year Year { get; }
+        public Month Month { get; }
+        public Day Day { get; }
+
+        public CCDateOnly(int year, int month, int day)
+        {
+            Year = new Year(year);
+            Month = new Month(month);
+            Day = new Day(day);
         }
     }
 
-    class Hour
+    public readonly record struct CCTimeOnly : IComparable<CCTimeOnly>
     {
-        public int Value { get; }
+        public Hour Hour { get; }
+        public Minute Minute { get; }
+        public Second Second { get; }
 
-        public Hour(int hour)
+        public CCTimeOnly(int hour, int minute, int second)
         {
-            if (hour < 0 || hour > 23)
-            {
-                throw new ArgumentOutOfRangeException(nameof(hour), "Hour must be between 0 and 23.");
-            }
-            Value = hour;
-        }
-    }
-
-    class Minute
-    {
-        public int Value { get; }
-
-        public Minute(int minute)
-        {
-            if (minute < 0 || minute > 59)
-            {
-                throw new ArgumentOutOfRangeException(nameof(minute), "Minute must be between 0 and 59.");
-            }
-            Value = minute;
-        }
-    }
-
-    class Second
-    {
-        public int Value { get; }
-
-        public Second(int second)
-        {
-            if (second < 0 || second > 59)
-            {
-                throw new ArgumentOutOfRangeException(nameof(second), "Second must be between 0 and 59.");
-            }
-            Value = second;
+            Hour = new Hour(hour);
+            Minute = new Minute(minute);
+            Second = new Second(second);
         }
     }
 }
