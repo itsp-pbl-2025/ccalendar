9a26590c22adab21ec2c0219f5e8ec3278b85f73
hibiki-isct <hibikisato0813@gmail.com>
2025-06-17 16:07:04 +0900

    祝日のキャッシュと祝日判定と祝日名取得機能の追加
    # Conflicts:
    #	Assets/Scripts/AppCore/UseCases/HolidayService.cs

--- Assets/Scripts/AppCore/UseCases/HolidayService.cs
+++ Assets/Scripts/AppCore/UseCases/HolidayService.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using AppCore.Interfaces;
-using AppCore.Utilities;
 using Cysharp.Threading.Tasks;
 using Domain.Api;
+using WebRequest;
 
 namespace AppCore.UseCases
 {
@@ -10,6 +10,8 @@ namespace AppCore.UseCases
     {
         private readonly IScheduleRepository _scheduleRepo;
         private readonly RequestHandler _api;
+        
+        private Dictionary<string, string> _holidayMap = new();
         
         public string Name { get; }
         
@@ -23,13 +25,45 @@ namespace AppCore.UseCases
 
         public async UniTask<HolidayList> GetHolidays(DateTime startDate, DateTime endDate)
         {
-            return await LoadHolidays(startDate, endDate);
+            var response = await LoadHolidays(startDate, endDate).ToUniTask();
+            if (response.error) return null;
+
+            CacheHolidayData(response.result);
+            return response.result;
         }
 
-        private async UniTask<HolidayList> LoadHolidays(DateTime startDate, DateTime endDate)
+        private ResponseHandler<HolidayList> LoadHolidays(DateTime startDate, DateTime endDate)
         {
-            var result = await _api.GetAsync<HolidayList>($"holidays?from={startDate:yyyy-MM-dd}&to={endDate:yyyy-MM-dd}");
-            return result.Data;
+            return new ResponseHandler<HolidayList>(
+                _api.Get($"holidays?from={startDate:yyyy-MM-dd}&to={endDate:yyyy-MM-dd}"));
+        }
+        
+        private void CacheHolidayData(HolidayList list)
+        {
+            _holidayMap.Clear();
+            foreach (var h in list.holidays)
+            {
+                _holidayMap[h.date] = h.name;
+            }
+        }
+        
+        public bool IsHoliday(DateTime date)
+        {
+            string key = date.ToString("yyyy-MM-dd");
+            return _holidayMap.ContainsKey(key);
+        }
+        
+        public string GetHolidayName(DateTime date)
+        {
+            string key = date.ToString("yyyy-MM-dd");
+    
+            if (_holidayMap.TryGetValue(key, out string name))
+            {
+                // "休日" の場合は "振替休日" に変換して返す
+                return name == "休日" ? "振替休日" : name;
+            }
+
+            return null;
         }
         
         public void Dispose()
