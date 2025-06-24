cc291484588d8b8a073ea2ec263cb792dea82a55
Daiki Usami <d_usami@no9.co.jp>
2025-06-24 16:10:40 +0900

    CCDateTime のテスト追加

---
+++ Assets/Scripts/Test/Unit/TestCCDateTime.cs
@@ -0,0 +1,59 @@
+using System;
+using Domain.Entity;
+using NUnit.Framework;
+
+namespace Test.Unit
+{
+    public class TestCCDateTime
+    {
+        [Test]
+        public void TestConversionCCDateTimeAndDateTime()
+        {
+            var dateTime = new DateTime(2025, 6, 24, 15, 30, 0);
+
+            var ccDateTime = new CCDateTime(dateTime);
+
+            Assert.AreEqual(ccDateTime.ToDateTime(), dateTime);
+        }
+
+        [Test]
+        public void TestCompareTo()
+        {
+            var ccDateTime1 = new CCDateTime(2025, 6, 24, 15, 30, 0);
+            var ccDateTime2 = new CCDateTime(2025, 6, 24, 15, 31, 0);
+
+            var result = ccDateTime1.CompareTo(ccDateTime2);
+
+            Assert.Less(result, 0);
+        }
+
+        [Test]
+        public void TestOperatorGreaterThan()
+        {
+            var earlier = new CCDateTime(new DateTime(2025, 6, 24, 15, 30, 0));
+            var later = new CCDateTime(new DateTime(2025, 6, 24, 15, 31, 0));
+
+            Assert.That(later > earlier, Is.True);
+        }
+
+        [Test]
+        public void TestOperatorLessThan()
+        {
+            var earlier = new CCDateTime(2025, 6, 24, 15, 30, 0);
+            var later = new CCDateTime(2025, 6, 24, 15, 31, 0);
+
+            Assert.That(earlier < later, Is.True);
+        }
+
+        [Test]
+        public void TestAddTime()
+        {
+            Assert.AreEqual(new CCDateTime(2025, 11, 30, 23, 59, 59).AddYears(2), new DateTime(2027, 11, 30, 23, 59, 59));
+            Assert.AreEqual(new CCDateTime(2025, 11, 30, 23, 59, 59).AddMonths(2), new DateTime(2026, 1, 30, 23, 59, 59));
+            Assert.AreEqual(new CCDateTime(2025, 11, 30, 23, 59, 59).AddDays(2), new CCDateTime(2025, 12, 1, 23, 59, 59));
+            Assert.AreEqual(new CCDateTime(2025, 11, 30, 23, 59, 59).AddHours(2), new DateTime(2025, 11, 31, 1, 59, 59));
+            Assert.AreEqual(new CCDateTime(2025, 11, 30, 23, 59, 59).AddMinutes(2), new DateTime(2025, 11, 31, 0, 1, 0));
+            Assert.AreEqual(new CCDateTime(2025, 11, 30, 23, 59, 59).AddSeconds(2), new DateTime(2025, 11, 31, 0, 0, 1));
+        }
+    }
+}
