f7c96cedccccefab9a4121f172549b97003ff0aa
hirano-d-titech <hirano.d.ae@m.titech.ac.jp>
2025-06-24 11:01:21 +0900

    feat: エディター上でのみ見える要素のためのクラスを実装した

---
+++ Assets/Scripts/Presentation/Views/Common/DebugSelfDestructor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Presentation.Views.Common
+{
+    public class DebugSelfDestructor : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
