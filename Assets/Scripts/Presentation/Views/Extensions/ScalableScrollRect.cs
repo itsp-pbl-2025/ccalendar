fbc43b75a87d5d1cc319203021664c0cc39d3742
hirano-d-titech <hirano.d.ae@m.titech.ac.jp>
2025-06-30 09:14:48 +0900

    wip: scalable scroll rect

---
+++ Assets/Scripts/Presentation/Views/Extensions/ScalableScrollRect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using UnityEngine.UI;
+
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+
+namespace Presentation.Views.Extensions
+{
+    public class OneFingerScrollRect : ScrollRect
+    {
+        public override void OnInitializePotentialDrag(PointerEventData eventData)
+        {
+            base.OnInitializePotentialDrag(eventData);
+        }
+
+        public override void OnBeginDrag(PointerEventData eventData)
+        {
+            if (Input.touchCount > 1) return;
+            base.OnBeginDrag(eventData);
+        }
+
+        public override void OnDrag(PointerEventData eventData)
+        {
+            if (Input.touchCount > 1) return;
+            base.OnDrag(eventData);
+        }
+    }
+
+}
