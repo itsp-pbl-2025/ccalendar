e213a7be2053361bc58b384a2431220712325595
hirano-d-titech <hirano.d.ae@m.titech.ac.jp>
2025-07-01 09:13:17 +0900

    wip: scaling test in mobile

---
+++ Assets/Scripts/Presentation/Utilities/VectorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Presentation.Utilities
+{
+    public static class VectorExtensions
+    {
+        public static Vector2 Abs(this Vector2 vec2)
+        {
+            return new Vector2(Mathf.Abs(vec2.x), Mathf.Abs(vec2.y));
+        }
+
+        public static Vector3 Abs(this Vector3 vec3)
+        {
+            return new Vector3(Mathf.Abs(vec3.x), Mathf.Abs(vec3.y), Mathf.Abs(vec3.z));
+        }
+    }
+}
--- Assets/Scripts/Presentation/Views/Extensions/ScalableScrollRect.cs
+++ Assets/Scripts/Presentation/Views/Extensions/ScalableScrollRect.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using Presentation.Presenter;
 using Presentation.Utilities;
+using R3;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -18,17 +20,17 @@ namespace Presentation.Views.Extensions
 {
     public class ScalableScrollRect : ScrollRect
     {
-        [SerializeField] protected RectTransform centerContent;
+        [SerializeField, Range(1f, 10f)] protected float scaleMax = 4f;
         
         #if UNITY_EDITOR
         [CustomEditor(typeof(ScalableScrollRect))]
         [CanEditMultipleObjects]
         public class ScalableScrollRectEditor : ScrollRectEditor
         {
-            private SerializedProperty _centerContentProp;
+            private SerializedProperty _scaleMaxProp;
             protected override void OnEnable()
             {
-                _centerContentProp = serializedObject.FindProperty("centerContent");
+                _scaleMaxProp = serializedObject.FindProperty("scaleMax");
                 base.OnEnable();
             }
 
@@ -38,7 +40,7 @@ namespace Presentation.Views.Extensions
                 
                 EditorGUILayout.LabelField("カーソルの中心を指すオブジェクト。", EditorStyles.label);
                 EditorGUI.BeginChangeCheck();
-                EditorGUILayout.PropertyField(_centerContentProp);
+                EditorGUILayout.PropertyField(_scaleMaxProp);
                 if (EditorGUI.EndChangeCheck())
                 {
                     serializedObject.ApplyModifiedProperties();
@@ -52,39 +54,101 @@ namespace Presentation.Views.Extensions
         }
 #endif
         
-        private readonly HashSet<int> _pointerTouches = new();
+        private readonly Dictionary<int, int> _pointerToTouch = new(), _touchToPointer = new();
+        private readonly ReactiveProperty<int> _pointersCount = new(0);
+
+        private float _currentScale, _standardHeight;
         
         public override void OnInitializePotentialDrag(PointerEventData eventData)
         {
-            _pointerTouches.Add(eventData.pointerId);
             base.OnInitializePotentialDrag(eventData);
         }
 
         public override void OnBeginDrag(PointerEventData eventData)
         {
+            // すでに登録されているポインターIDは蹴る
+            if (_pointerToTouch.ContainsKey(eventData.pointerId)) return;
+            
+            // 入力が検知されたPointerに最も近いまだ登録されていないTouchを、相互参照登録する
+            var bestTouchId = -1;
+            var bestDist = float.MaxValue;
+            foreach (var t in Touch.activeTouches)
+            {
+                if (_touchToPointer.ContainsKey(t.touchId)) continue;
+                // positionはどちらもスクリーン座標系らしい、ChatGPTによると
+                var d = Vector2.SqrMagnitude(t.screenPosition - eventData.position);
+                if (d >= bestDist) continue;
+                bestDist = d;
+                bestTouchId = t.touchId;
+            }
+
+            if (bestTouchId >= 0)
+            {
+                _pointerToTouch[eventData.pointerId] = bestTouchId;
+                _touchToPointer[bestTouchId] = eventData.pointerId;
+                _pointersCount.Value = _pointerToTouch.Count;
+            }
+
             base.OnBeginDrag(eventData);
         }
 
-        public override void OnDrag(PointerEventData eventData)
+        public override void OnEndDrag(PointerEventData eventData)
         {
-            _pointerTouches.Remove(eventData.pointerId);
-            base.OnDrag(eventData);
+            // 手を離したタイミングで pointerId と touchId の連携を解く
+            _touchToPointer.Remove(_pointerToTouch[eventData.pointerId]);
+            _pointerToTouch.Remove(eventData.pointerId);
+            _pointersCount.Value = _pointerToTouch.Count;
+            base.OnEndDrag(eventData);
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            _currentScale = 1f;
+            _standardHeight = content.sizeDelta.y;
+            
+            _pointersCount.Subscribe(_ =>
+            {
+                _standardHeight = content.sizeDelta.y / _currentScale;
+            }).AddTo(this);
         }
 
         private void Update()
         {
-            if (_pointerTouches.Count > 0)
+            if (!Application.isPlaying) return;
+            
+            var touches = Touch.activeTouches.AsValueEnumerable().Where(t => _touchToPointer.ContainsKey(t.touchId));
+            if (touches.Count() >= 2)
             {
-                var touches = Touch.activeTouches.AsValueEnumerable().Where(t => _pointerTouches.Contains(t.touchId));
-                var touchStr = Touch.activeTouches.Count == 0 ? "null" : Touch.activeTouches.AsValueEnumerable().Select(x => x.touchId.ToString()).Aggregate((x, y) => $"{x}, {y}");
-                var pointStr = _pointerTouches.Count == 0 ? "null" : _pointerTouches.AsValueEnumerable().Select(x => x.ToString()).Aggregate((x, y) => $"{x}, {y}");
-                Debug.Log($"activeTouchIds: {touchStr}, pointerTouches: {pointStr}");
-                if (touches.Count() > 0)
-                {
-                    var center = touches.Select(touch => touch.screenPosition).Average();
-                    centerContent.anchoredPosition = center;
-                }
+                var center = touches.Select(touch => 
+                    RectTransformUtility.ScreenPointToLocalPointInRectangle(viewport, touch.screenPosition,
+                        InAppContext.SceneLoader.Camera, out var positionInRect) ? positionInRect : Vector2.zero)
+                    .Average();
+                var absDiff = touches.Select(touch => 
+                    ((RectTransformUtility.ScreenPointToLocalPointInRectangle(viewport, touch.screenPosition,
+                        InAppContext.SceneLoader.Camera, out var currentInRect) ? currentInRect : Vector2.zero) - center).Abs() 
+                    - ((RectTransformUtility.ScreenPointToLocalPointInRectangle(viewport, touch.startScreenPosition,
+                        InAppContext.SceneLoader.Camera, out var startInRect) ? startInRect : Vector2.zero) - center).Abs())
+                    .Sum();
+                
+                _currentScale = (content.sizeDelta + absDiff).y / content.sizeDelta.y;
+                
+                content.sizeDelta = 
+                    content.sizeDelta = new Vector2(content.sizeDelta.x, _standardHeight * _currentScale);
             }
+            else
+            {
+                _currentScale = 1f;
+            }
+            
+            AdjustContent();
+        }
+
+        private void AdjustContent()
+        {
+            content.sizeDelta = new Vector2(content.sizeDelta.x, 
+                Mathf.Clamp(content.sizeDelta.y, viewport.rect.size.y, viewport.rect.size.y * scaleMax));
         }
     }
 }
