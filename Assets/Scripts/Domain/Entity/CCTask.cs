6ffe1f52d27ea9cae2a67cd098fc74ce5387fe87
SotaOgawa <ogawa.s.ay@m.titech.ac.jp>
2025-07-01 16:29:33 +0900

    Taskに推定所要時間を設定

--- Assets/Scripts/Domain/Entity/CCTask.cs
+++ Assets/Scripts/Domain/Entity/CCTask.cs
@@ -2,12 +2,15 @@ using System;
 
 namespace Domain.Entity
 {
-    public record CCTask(int Id, string Title, string Description, int Priority, DateTime Deadline)
+    public record CCTask(int Id, string Title, string Description, int Priority, DateTime Deadline, TimeSpan Duration)
     {
+        public CCTask(int Id, string Title, string Description, int Priority, DateTime Deadline)
+            : this(Id, Title, Description, Priority, Deadline, TimeSpan.FromHours(1)){}
         public int Id { get; } = Id;
         public string Title { get; } = Title;
         public string Description { get; } = Description;
         public int Priority { get; } = Priority;
+        public TimeSpan Duration { get; } = Duration; 
         public DateTime Deadline { get; } = Deadline;
     }
 }
