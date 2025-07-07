508a49df45a371bf1fccfcfa98c8aee8aa7584c8
hirano-d-titech <hirano.d.ae@m.titech.ac.jp>
2025-07-07 15:00:59 +0900

    improve: スクロールの最初で移動軸を決めてそれ以外の方向に移動できないように

--- Assets/Scripts/Presentation/Views/Extensions/ScalableScrollRect.cs
+++ Assets/Scripts/Presentation/Views/Extensions/ScalableScrollRect.cs
@@ -20,7 +20,7 @@ namespace Presentation.Views.Extensions
         [SerializeField, Range(1f, 10f)] protected float scaleMax = 4f;
         [SerializeField] private RectTransform sideContent;
         
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         [CustomEditor(typeof(ScalableScrollRect))]
         [CanEditMultipleObjects]
         public class ScalableScrollRectEditor : ScrollRectEditor
@@ -58,7 +58,12 @@ namespace Presentation.Views.Extensions
         private readonly Dictionary<int, int> _pointerToTouch = new(), _touchToPointer = new();
         private readonly ReactiveProperty<int> _pointersCount = new(0);
 
+        // vertical settings
         private float _currentScale, _standardHeight;
+        // horizontal settings
+        private float _backVelocity;
+        private bool _horizontalMoved;
+        
         private int _scrollingPointerId = -1;
         
         public override void OnInitializePotentialDrag(PointerEventData eventData)
@@ -89,6 +94,15 @@ namespace Presentation.Views.Extensions
                 _pointerToTouch[eventData.pointerId] = bestTouchId;
                 _touchToPointer[bestTouchId] = eventData.pointerId;
                 _pointersCount.Value = _pointerToTouch.Count;
+                
+                // これが一つ目のポインターなら、移動方向を見て確定する
+                if (_pointersCount.Value is 1)
+                {
+                    var v = Touch.activeTouches[0].delta;
+                    var xAdv = Mathf.Abs(v.x) > Mathf.Abs(v.y);
+                    horizontal = xAdv;
+                    vertical = !xAdv;
+                }
             }
 
             if (_pointersCount.Value == 1)
@@ -114,6 +128,13 @@ namespace Presentation.Views.Extensions
                 _scrollingPointerId = -1;
                 base.OnEndDrag(eventData);
             }
+
+            // ポインターが全部離れたら、移動方向制限を解除する
+            if (_pointersCount.Value is 0)
+            {
+                horizontal = true;
+                vertical = true;
+            }
         }
 
         protected override void Awake()
@@ -132,32 +153,76 @@ namespace Presentation.Views.Extensions
         private void Update()
         {
             if (!Application.isPlaying) return;
-            
-            var touches = Touch.activeTouches.AsValueEnumerable().Where(t => _touchToPointer.ContainsKey(t.touchId));
-            if (touches.Count() >= 2)
+
+            if (vertical)
             {
-                var center = touches.Select(touch => 
-                    RectTransformUtility.ScreenPointToLocalPointInRectangle(viewport, touch.screenPosition,
-                        InAppContext.SceneLoader.Camera, out var positionInRect) ? positionInRect : Vector2.zero)
-                    .Average();
-                var absDiff = touches.Select(touch => 
-                    ((RectTransformUtility.ScreenPointToLocalPointInRectangle(viewport, touch.screenPosition,
-                        InAppContext.SceneLoader.Camera, out var currentInRect) ? currentInRect : Vector2.zero) - center).Abs() 
-                    - ((RectTransformUtility.ScreenPointToLocalPointInRectangle(viewport, touch.startScreenPosition,
-                        InAppContext.SceneLoader.Camera, out var startInRect) ? startInRect : Vector2.zero) - center).Abs())
-                    .Sum();
-                
-                _currentScale = (content.sizeDelta + absDiff).y / content.sizeDelta.y;
-                
-                content.sizeDelta = 
-                    content.sizeDelta = new Vector2(content.sizeDelta.x, _standardHeight * _currentScale);
+                var touches = Touch.activeTouches.AsValueEnumerable()
+                    .Where(t => _touchToPointer.ContainsKey(t.touchId));
+                if (touches.Count() >= 2)
+                {
+                    var center = touches.Select(touch =>
+                            RectTransformUtility.ScreenPointToLocalPointInRectangle(viewport, touch.screenPosition,
+                                InAppContext.SceneLoader.Camera, out var positionInRect)
+                                ? positionInRect
+                                : Vector2.zero)
+                        .Average();
+                    var absDiff = touches.Select(touch =>
+                            ((RectTransformUtility.ScreenPointToLocalPointInRectangle(viewport, touch.screenPosition,
+                                InAppContext.SceneLoader.Camera, out var currentInRect)
+                                ? currentInRect
+                                : Vector2.zero) - center).Abs()
+                            - ((RectTransformUtility.ScreenPointToLocalPointInRectangle(viewport,
+                                touch.startScreenPosition,
+                                InAppContext.SceneLoader.Camera, out var startInRect)
+                                ? startInRect
+                                : Vector2.zero) - center).Abs())
+                        .Sum();
+
+                    _currentScale = (content.sizeDelta + absDiff).y / content.sizeDelta.y;
+
+                    content.sizeDelta =
+                        content.sizeDelta = new Vector2(content.sizeDelta.x, _standardHeight * _currentScale);
+                }
+                else
+                {
+                    _currentScale = 1f;
+                }
             }
-            else
+
+            if (horizontal)
             {
-                _currentScale = 1f;
+                if (_pointersCount.Value is 0)
+                {
+                    if (_horizontalMoved)
+                    {
+                        _backVelocity -= ReversingVelocity(content.anchoredPosition.x);
+                        content.anchoredPosition += _backVelocity * Time.deltaTime * Vector2.right;
+                        if ((int)Mathf.Sign(_backVelocity) == (int)Mathf.Sign(content.anchoredPosition.x) ||
+                            Mathf.Abs(content.anchoredPosition.x) < 1f)
+                        {
+                            content.anchoredPosition = new Vector2(0f, content.anchoredPosition.y);
+                            _backVelocity = 0f;
+                            _horizontalMoved = false;
+                        }
+                    }
+                    else
+                    {
+                        content.anchoredPosition = new Vector2(0f, content.anchoredPosition.y);
+                    }
+                }
+                else
+                {
+                    _horizontalMoved = true;
+                }
             }
             
             AdjustContent();
+            return;
+
+            float ReversingVelocity(float offset)
+            {
+                return (Mathf.Sqrt(Mathf.Abs(offset) + 1000) * 100 * Mathf.Sign(offset)) * Time.deltaTime;
+            }
         }
 
         private void AdjustContent()
