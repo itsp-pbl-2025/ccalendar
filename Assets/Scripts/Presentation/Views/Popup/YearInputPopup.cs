44c19d68272d0526a123d58bea7e6294afe04b12
hibiki-isct <209947427+hibiki-isct@users.noreply.github.com>
2025-07-08 15:35:22 +0900

    年選択のポップアップを作成。(テストは作成中)

---
+++ Assets/Scripts/Presentation/Views/Popup/YearInputPopup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Presentation.Views.Popup
+{
+    public class YearInputPopup : PopupWindow
+    {
+        [SerializeField] private GameObject inputFieldContainer; // 入力モード用のオブジェクト
+        [SerializeField] private TMP_InputField yearInputField;
+        [SerializeField] private TextMeshProUGUI yearLabel;
+        [SerializeField] private Button positiveButton, negativeButton;
+        [SerializeField] private Button yearLabelButton; // 中央のLabeledRoundButton
+        [SerializeField] private Button yearDecrementButton, yearIncrementButton;
+        [SerializeField] private Button frameBackgroundButton; // CommonPopupFrame の背景
+
+        private Action<int> _onYearDefined;
+        private int _currentYear;
+        private bool _isInputMode;
+
+        private void Start()
+        {
+            // 仮のコールバックと現在年で初期化
+            Init(year => Debug.Log($"年が選択されました: {year}"));
+        }
+        
+        public void Init(Action<int> onYearDefined, int? initialYear = null)
+        {
+            _onYearDefined = onYearDefined;
+            _currentYear = initialYear ?? DateTime.Now.Year;
+
+            yearInputField.characterLimit = 4;
+            yearInputField.contentType = TMP_InputField.ContentType.IntegerNumber;
+
+            SetYear(_currentYear);
+            SetupListeners();
+            SetInputMode(false);
+        }
+
+        private void SetupListeners()
+        {
+            positiveButton.onClick.AddListener(OnPressDefineButton);
+            negativeButton.onClick.AddListener(CloseWindow);
+            yearDecrementButton.onClick.AddListener(() => ChangeYear(-1));
+            yearIncrementButton.onClick.AddListener(() => ChangeYear(1));
+            yearLabelButton.onClick.AddListener(() => SetInputMode(true));
+            frameBackgroundButton.onClick.AddListener(() => { if (_isInputMode) ApplyInputAndReturn(); });
+        }
+
+        private void SetYear(int year)
+        {
+            _currentYear = Mathf.Clamp(year, 1, 9999);
+            yearLabel.text = _currentYear.ToString();
+            yearInputField.text = _currentYear.ToString();
+        }
+
+        private void ChangeYear(int delta)
+        {
+            SetYear(_currentYear + delta);
+        }
+
+        private void SetInputMode(bool inputMode)
+        {
+            _isInputMode = inputMode;
+            inputFieldContainer.SetActive(inputMode);
+            yearLabel.gameObject.SetActive(!inputMode);
+        }
+
+        private void ApplyInputAndReturn()
+        {
+            if (int.TryParse(yearInputField.text, out int inputYear))
+            {
+                SetYear(inputYear);
+            }
+            SetInputMode(false);
+        }
+
+        private void OnPressDefineButton()
+        {
+            if (_isInputMode)
+            {
+                ApplyInputAndReturn();
+            }
+            _onYearDefined?.Invoke(_currentYear);
+            CloseWindow();
+        }
+    }
+}
