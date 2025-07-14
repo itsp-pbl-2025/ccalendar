ca3ef97194452e5c2142ff9c76a54d688919843c
hirano-d-titech <hirano.d.ae@m.titech.ac.jp>
2025-07-14 21:12:02 +0900

    feat: いろんなものの文字列化

---
+++ Assets/Scripts/AppCore/Utilities/ScheduleExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Entity;
+using Domain.Enum;
+using ZLinq;
+
+namespace AppCore.Utilities
+{
+    public static class ScheduleExtensions
+    {
+        public static string ToString(this SchedulePeriodic periodic)
+        {
+            const string noRepeat = "繰り返しなし";
+
+            var span = periodic.Span;
+            switch (periodic.PeriodicType)
+            {
+                case SchedulePeriodicType.EveryDay:
+                    if (span is 1)
+                    {
+                        return "毎日";
+                    }
+                    return $"{span}日に1回";
+                case SchedulePeriodicType.EveryWeekday:
+                    var weekdays = new List<DayOfWeek>();
+                    foreach (DayOfWeek day in System.Enum.GetValues(typeof(DayOfWeek)))
+                    {
+                        if ((span & (1 << (int)day)) != 0)
+                        {
+                            weekdays.Add(day);
+                        }
+                    }
+                    if (weekdays.Count is 0) return noRepeat;
+                    if (weekdays.Count is 7) return "毎日";
+                    return $"毎週{string.Join(',', weekdays.AsValueEnumerable().Select(d => d.ToShortString()))}曜日";
+                case SchedulePeriodicType.EveryWeek:
+                    return "毎週";
+                case SchedulePeriodicType.EveryMonth:
+                    if (span >= 100)
+                    {
+                        var index = span % 100;
+                        var day = (DayOfWeek)(span / 100);
+                        return $"{(index > 4 ? "最終" : $"第{index}")}{day.ToLongString()}";
+                    }
+                    return $"毎月{span}日";
+                case SchedulePeriodicType.EveryYear:
+                    return $"{span}年に1回";
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
